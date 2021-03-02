0aafc86d7a5b0174070c60fd5395382d69cd8107
Jacob Borga <jdborga@gmail.com>
2021-03-02 20:26:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,5 @@ def display_board(board)
   col = "|"
   puts "#{board[0]}  #{col} #{board[1]}  #{col} #{board[2]} " + "\n" + row + "\n" +
        "#{board[3]}  #{col} #{board[4]}  #{col} #{board[5]} " + "\n" + row + "\n" +
-       "#{board[6]}  #{col} #{board[7]}  #{col} #{board[8]} "
+       "#{board[6]}  #{col} #{board[7]} #{col} #{board[8]} "
 end
